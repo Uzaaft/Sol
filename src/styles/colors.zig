c833a7abf0b2bdf98239095f16d70f10c39f8a11
rednur01 <rednur01@gmail.com>
2024-10-08 00:20:28 +0600

    feat(calendar): highlight active cell

--- src/styles/colors.zig
+++ src/styles/colors.zig
@@ -3,4 +3,5 @@ const vaxis = @import("vaxis");
 pub const borderColor: vaxis.Color = .{ .rgb = [3]u8{ 255, 255, 255 } };
 pub const activeBorderColor: vaxis.Color = .{ .rgb = [3]u8{ 0, 255, 0 } };
 
-pub const selectedBgCellColor: vaxis.Color = .{ .rbg = [3]u8{ 0, 255, 0 } };
+pub const selectedBgCellColor: vaxis.Color = .{ .rgb = [3]u8{ 0, 255, 0 } };
+pub const selectedFgCellColor: vaxis.Color = .{ .rgb = [3]u8{ 0, 0, 0 } };
